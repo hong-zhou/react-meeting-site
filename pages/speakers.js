71c13442e11b5ce51972cd13c205c5ca68b9b2d1
Hong Zhou <zhsnnu@gmail.com>
2020-07-05 09:57:39 -0500

    useContext to pass speakers data

--- pages/speakers.js
+++ pages/speakers.js
@@ -3,17 +3,32 @@ import Menu from "../src/components/Menu/Menu";
 import SpeakerSearchBar from "../src/components/SpeakerSearchBar/SpeakerSearchBar";
 import Speakers from "../src/components/Speakers/Speakers";
 import Footer from "../src/components/Footer/Footer";
+import SpeakerContext from "../src/components/Speakers/SpeakerContext";
 
-function Page() {
+export default function Page() {
+  const speakers = [
+    {
+      imageSrc: "speaker-component-1124",
+      name: "Douglas Crockford",
+    },
+    {
+      imageSrc: "speaker-component-1530",
+      name: "Tamara Baker",
+    },
+    {
+      imageSrc: "speaker-component-10803",
+      name: "Eugene Chuvyrov",
+    },
+  ];
   return (
     <div>
       <Header />
       <Menu />
-      <SpeakerSearchBar />
-      <Speakers />
+      <SpeakerContext.Provider value={speakers}>
+        <SpeakerSearchBar />
+        <Speakers />
+      </SpeakerContext.Provider>
       <Footer />
     </div>
   );
 }
-
-export default Page;
--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -1,20 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
+import SpeakerContext from "./SpeakerContext";
 
 const Speakers = () => {
-  const speakers = [
-    {
-      imageSrc: "speaker-component-1124",
-      name: "Douglas Crockford",
-    },
-    {
-      imageSrc: "speaker-component-1530",
-      name: "Tamara Baker",
-    },
-    {
-      imageSrc: "speaker-component-10803",
-      name: "Eugene Chuvyrov",
-    },
-  ];
+  const speakers = useContext(SpeakerContext);
   return (
     <div>
       {speakers.map(({ imageSrc, name }) => {
