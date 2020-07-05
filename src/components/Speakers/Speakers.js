20f9937a6c5521f50ef524f4ee016ece887ecdfb
Hong Zhou <zhsnnu@gmail.com>
2020-07-04 22:40:09 -0500

    Render props pattern

--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -1,29 +1,26 @@
 import React from "react";
+import SpeakersRenderProps from "./SpeakersRenderProps";
 
-const Speaker = () => {
-  const speakers = [
-    {
-      imageSrc: "speaker-component-1124",
-      name: "Douglas Crockford",
-    },
-    {
-      imageSrc: "speaker-component-1530",
-      name: "Tamara Baker",
-    },
-    {
-      imageSrc: "speaker-component-10803",
-      name: "Eugene Chuvyrov",
-    },
-  ];
+const Speakers = () => {
   return (
-    <div>
-      {speakers.map(({ imageSrc, name }) => {
+    <SpeakersRenderProps>
+      {({ speakers }) => {
         return (
-          <img src={`/images/${imageSrc}.png`} alt={name} key={imageSrc}></img>
+          <div>
+            {speakers.map(({ imageSrc, name }) => {
+              return (
+                <img
+                  src={`/images/${imageSrc}.png`}
+                  alt={name}
+                  key={imageSrc}
+                ></img>
+              );
+            })}
+          </div>
         );
-      })}
-    </div>
+      }}
+    </SpeakersRenderProps>
   );
 };
 
-export default Speaker;
+export default Speakers;
